62df20139eb76e0a5e031fc3290ddc36ef67db16
Pratisruti Sahoo <101540327+VU21CSEN0200116@users.noreply.github.com>
2022-04-08 09:34:16 +0530

    fivetable.c
    This program is used to calculate the table for 5

---
+++ fivetable.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int fivetable(int a,int i){
+    if(i==0)
+    return 0;
+    printf("%d * %d = %d\n",a,11-i,a*(11-i));
+    fivetable(a,--i);
+}
+int main()
+{
+    int a,size;
+    a=5;
+    size=10;
+    fivetable(a,size);
+    return 0;
+}
