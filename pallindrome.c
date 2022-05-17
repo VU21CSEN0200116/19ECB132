3e1600f847d1a31823334a1a04a56265f9481b64
Pratisruti Sahoo <101540327+VU21CSEN0200116@users.noreply.github.com>
2022-05-17 19:30:50 +0530

    Create pallindrome.c

---
+++ pallindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n,rem,rev=0,num;
+	printf("Enter your number \n");
+	scanf("%d",&n);
+	num=n;
+	while(n!=0){
+		rem=n%10;
+		rev=rev*10+rem;
+		n/=10;
+	}
+	printf("The reversed number is %d \n",rev);
+	if(num==rev){
+		printf("The number is a pallindrome");
+	}
+	else{
+		printf("The number is not a pallindrome");
+	}
+	return 0;
+}
