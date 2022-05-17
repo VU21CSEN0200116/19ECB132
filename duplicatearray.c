efced585c0daeac2ba9832eb7b9ca70a8ef0f459
Pratisruti Sahoo <101540327+VU21CSEN0200116@users.noreply.github.com>
2022-05-17 19:23:59 +0530

    Create duplicatearray.c

---
+++ duplicatearray.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+void inputarr(int arr[],int n);
+void checkdup(int arr[],int n);
+void printarr(int arr[],int n);
+int main(){
+	int i,n;
+	printf("enter the size \n");
+	scanf("%d",&n);
+	int *arr=(int*)malloc(n*sizeof(int));
+	inputarr(arr,n);
+	printarr(arr,n);
+	checkdup(arr,n);
+	return 0;
+}
+void inputarr(int arr[],int n){
+	int i;
+	printf("enter the elements \n");
+	for(i=0;i<n;i++){
+		printf("enter element %d :",i+1);
+		scanf("%d",&arr[i]);
+	}
+}
+void checkdup(int arr[],int n){
+	int i,g,count=0;
+	g=(arr[0]==arr[1])?(arr[1]):(arr[0]);
+	for(i=2;i<n;i++){
+		if(g!=arr[i]){
+			printf("%d ",arr[i]);
+		}
+		g=arr[i];
+		count+=count;
+	}
+}
+void printarr(int arr[],int n){
+	int i;
+	for(i=0;i<n;i++){
+		printf("%d ",arr[i]);
+	}
+}
