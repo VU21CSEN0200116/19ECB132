73acb0ad9246f4f76e845cb403425eacb29d1db8
Pratisruti Sahoo <101540327+VU21CSEN0200116@users.noreply.github.com>
2022-05-17 19:35:28 +0530

    Create transposearray.c

---
+++ transposearray.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+void inputarr(int arr[10][10],int n,int m);
+void transpose(int arr[10][10],int n,int m);
+void printarr(int arr[10][10],int n,int m);
+int main()
+{
+	int arr[10][10];
+	int n,m;
+	printf("enter the rows and coloumns \n");
+	scanf("%d %d",&m,&n);
+	inputarr(arr,n,m);
+	printarr(arr,n,m);
+	transpose(arr,n,m);
+	printarr(arr,m,n);
+	return 0;
+}
+void inputarr(int arr[10][10],int n,int m){
+	int i,j;
+	printf("enter your elements \n");
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			scanf("%d",&arr[i][j]);
+		}
+	}	
+}
+void transpose(int arr[10][10],int n,int m){
+	int i,j,c;
+	for(i=0;i<m;i++){
+		for(j=i;j<n;j++){
+			c=arr[i][j];
+		    arr[i][j]=arr[j][i];
+		    arr[j][i]=c;
+		}
+	}
+}
+void printarr(int arr[10][10],int n,int m){
+	int i,j;
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){
+			printf("%d ",arr[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
